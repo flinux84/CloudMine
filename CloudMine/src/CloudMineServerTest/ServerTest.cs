dc06995339ab23904a80dcc99a8117aae90f3861
flinux84 <linus.soderholm@gmail.com>
2016-11-23 10:19:18 +0100

    Projects created.

---
+++ CloudMine/src/CloudMineServerTest/ServerTest.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Xunit;
+
+namespace CloudMineServerTest
+{
+    public class ServerTest
+    {
+        //Use this class to write test for CloudMineServer
+
+        [Fact]
+        public void FirstTest()
+        {
+        }
+    }
+}
