3faa5a74b367803eacc24ad8228958e1b02928bf
flinux84 <linus.soderholm@gmail.com>
2016-12-07 12:51:32 +0100

    drag n drop

--- CloudMine/src/CloudMineServer/wwwroot/js/draganddrop.js
+++ CloudMine/src/CloudMineServer/wwwroot/js/draganddrop.js
@@ -1,23 +1,31 @@
-﻿var obj = $("#container");
-obj.on('dragenter', function (e) {
-    e.stopPropagation();
-    e.preventDefault();
-    $(this).css('border', '2px solid #0B85A1');
-});
-obj.on('dragover', function (e) {
-    e.stopPropagation();
-    e.preventDefault();
-});
-obj.on('drop', function (e) {
+﻿var dropzone;
 
-    $(this).css('border', '2px dotted #0B85A1');
-    e.preventDefault();
-    var files = e.originalEvent.dataTransfer.files;
+$(document).ready(function () {
 
-    //We need to send dropped files to Server
-    handleFileUpload(files, obj);
+dropzone = $("#dropzone")
+
+    dropzone.on('dragenter', function (e) {
+        e.preventDefault();
+        e.stopPropagation();
+        $(this).css('border', '2px solid #0B85A1');
+    });
+
+    dropzone.on('dragover', function (e) {
+        e.preventDefault();
+        e.stopPropagation();
+    });
+
+    dropzone.on('drop', function (e) {
+
+        $(this).css('border', '2px dotted #0B85A1');
+        e.preventDefault();
+        e.stopPropagation();
+
+        var files = e.originalEvent.dataTransfer.files;
+
+        dragDropUpload(files, dropzone);
+    });
 });
-
 
 $(document).on('dragenter', function (e) {
     e.stopPropagation();
@@ -26,7 +34,7 @@ $(document).on('dragenter', function (e) {
 $(document).on('dragover', function (e) {
     e.stopPropagation();
     e.preventDefault();
-    obj.css('border', '2px dotted #0B85A1');
+    dropzone.css('border', '2px dotted #0B85A1');
 });
 $(document).on('drop', function (e) {
     e.stopPropagation();
@@ -34,14 +42,14 @@ $(document).on('drop', function (e) {
 });
 
 
-function handleFileUpload(files, obj) {
+function dragDropUpload(files, dropzone) {
     for (var i = 0; i < files.length; i++) {
         var fd = new FormData();
         fd.append('file', files[i]);
+        console.log("Du försöker ladda upp " + files[i].name)
+        //var status = new createStatusbar(obj); //Using this we can set progress.
+        //status.setFileNameSize(files[i].name, files[i].size);
 
-        var status = new createStatusbar(obj); //Using this we can set progress.
-        status.setFileNameSize(files[i].name, files[i].size);
-        sendFileToServer(fd, status);
-
+        //sendFileToServer(fd, status); anropa uploaden i Ali/Joakims kod, ändra när den är klar.
     }
 }
