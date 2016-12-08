aabd8e45d82fc5c3bbc638f592aff36e34b1f4e2
flinux84 <linus.soderholm@gmail.com>
2016-12-08 19:49:07 +0100

    kommenterade koden lite. ReturnFileController är just nu downloader-funktionen på serversidan. När denna är helt klar så ska den in i det vanliga API'et, men för närvarande får den ligga separerad.

--- CloudMine/src/CloudMineServer/API-server/Controllers/ReturnFileController.cs
+++ CloudMine/src/CloudMineServer/API-server/Controllers/ReturnFileController.cs
@@ -20,20 +20,33 @@ namespace CloudMineServer.API_server.Controllers
             _context = context;
         }
 
+
+        //Denna fungerar enligt "gamla metoden". Filen skapas på servern och returneras till klienten.
+        //dock ligger filen kvar på servern så den tas inte bort, kanske går att lägga till funktion för det.
         [HttpGet("{id:int}")]
         public async Task<FileStreamResult> Get([FromRoute] int id)
         {
             var merge = new FileMerge();
             var userId = User.GetUserId();
 
-            var stream = System.IO.File.OpenRead("D:\\Schoolprojects\\CloudMine\\CloudMine\\src\\CloudMineServer\\Temp\\StorageExplorer.exe");
+            var file = await _context.GetSpecifikFileItemAndDataChunk(id, userId);
+
+            var merger = new FileMerge();
+            var uri = merger.MakeFileOnServer(file);
+            
+            var stream = System.IO.File.OpenRead(uri.AbsolutePath);
 
             return new FileStreamResult(stream, new MediaTypeHeaderValue("application/octet-stream")) {
-                FileDownloadName = "StorageExplorer.exe"
+                FileDownloadName = file.FileName
             };
             
         }
 
+        //Martins förslag bygger på att man använder en ny returtyp som ärver FileResult.
+        //Denna har jag lagt i services och heter FileCallbackResult och ska kunna hantera chunks av streams.
+        //Så då skulle man isåfall implementera en annan Get-metod som använder denna och vi skippar isåfall att använda FileMerge().
+        //Länk att läsa på detta: http://blog.stephencleary.com/2016/11/streaming-zip-on-aspnet-core.html
+        // i länken så skapar han upp en zip-fil "on the fly", men vi får anpassa den isåfall.
 
     }
 }
--- CloudMine/src/CloudMineServer/wwwroot/js/appendhtml.js
+++ CloudMine/src/CloudMineServer/wwwroot/js/appendhtml.js
@@ -8,8 +8,7 @@ var HTMLappender = function (element) {
     HTMLappender.prototype.appendTable = function (result) {
         var i;
         for(i = 0; i < result.length; i++){
-            table.append('<tr><td>' + result[i].id
-                + '</td><td>' + result[i].fileName
+            table.append('<tr><td>' + result[i].fileName
                 + '</td><td>' + result[i].fileSize
                 + '</td><td>' + result[i].uploaded.split('T')[0]
                 + '</td><td>' + result[i].dataType
