87b61e5afe3e1da98e3a5e8728aa6095533a1446
flinux84 <linus.soderholm@gmail.com>
2016-12-15 09:30:20 +0100

    new bool IsComplete

---
+++ CloudMine/src/CloudMineServer/Migrations/CloudDbRepositoryMigrations/20161215082818_IsComplete.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace CloudMineServer.Migrations.CloudDbRepositoryMigrations
+{
+    public partial class IsComplete : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.RenameColumn(
+                name: "Private",
+                table: "FileItems",
+                newName: "IsComplete");
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.RenameColumn(
+                name: "IsComplete",
+                table: "FileItems",
+                newName: "Private");
+        }
+    }
+}
